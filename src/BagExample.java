bb8e60a0cf18672fa389bb28338ab667a9818b0c
Chrisnguyen3 <chrisnguyen3@cmail.carleton.ca>
2019-11-11 09:14:34 -0500

    Converted type to top Level

--- src/BagExample.java
+++ src/BagExample.java
@@ -1,19 +1,5 @@
 
 public class BagExample {
-	private final class BagImpl implements Bag {
-		Object o;
-
-		public Object get()
-		 {
-		    return o;
-		 }
-
-		public void set(Object o)
-		 {
-		    this.o = o;
-		 }
-	}
-
 	void processMessage(String msg)
 	   {
 	      Bag bag = new BagImpl();
